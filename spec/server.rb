55d7a3346ec9d42442e8eeab5d1f8206f6c28e1c
Aaron Brown <aaron@9minutesnooze.com>
2012-11-13 10:50:18 -0500

    tests for IdeeliSpinup::Server class

---
+++ spec/server.rb
@@ -0,0 +1,36 @@
+require 'fog'
+require 'ideeli_spinup'
+
+describe IdeeliSpinup::Server do
+  before do
+    Fog.mock!
+    options = { :region            => 'us-east-1',
+                :security_group    => 'default' }
+ 
+    config = { :accounts => { 'account' => 
+               { :aws_access_key_id     =>"ABCDEFG",
+                 :aws_secret_access_key =>"ABCDEFG" } } }
+
+    @env = IdeeliSpinup::Environment.new(config, options)
+    
+  end
+
+  describe '#calc_az_from_hostname' do
+    context 'has number in hostname' do
+      let(:server) { IdeeliSpinup::Server.new('foo12.bar.com', @env) }
+
+      it 'should be in us-east-1b' do
+        server.calc_az_from_hostname.should == 'us-east-1b'
+      end
+    end
+
+    context 'no number in hostname' do
+      let(:server) { IdeeliSpinup::Server.new('foo.bar.com', @env) }
+
+      it 'should be in us-east-1a' do
+        server.calc_az_from_hostname.should == 'us-east-1a'
+      end
+    end
+    
+  end
+end
